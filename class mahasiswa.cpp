9add6324fe528e9ccd58afae6bedc17e617c74d5
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-04-23 15:19:01 +0700

    add metod prosedur output

--- class mahasiswa.cpp
+++ class mahasiswa.cpp
@@ -27,5 +27,14 @@ string namaMk;
               cout << "SKS : ";
               cin >> sks;
           }
-}
+           void output() {
+              cout << "Kode MK : " << kodeMk << endl;
+              cout << "Nama MK : " << namaMk << endl;
+              cout << "SKS : " << sks << endl; 
+          }
+};
+
+
+
+
 
