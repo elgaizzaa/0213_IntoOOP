5bc581a56c2b5a2ad6f7f363e0ea29d67dcfc7e6
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-04-23 15:17:54 +0700

    add metod prosedur input

--- class mahasiswa.cpp
+++ class mahasiswa.cpp
@@ -19,7 +19,13 @@ string kodeMk;
 string namaMk;
  int sks;
  public:
-
-
+ void input() {
+    cout << "Kode MK: ";
+              cin >> kodeMk;
+              cout << "Nama MK : ";
+              cin >> namaMk;
+              cout << "SKS : ";
+              cin >> sks;
+          }
 }
 
